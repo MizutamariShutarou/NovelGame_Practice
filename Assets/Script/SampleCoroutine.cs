8ad55205aaa0b4b27fc89ad6a0b8119bd81ab207
MizutamariShutaro <shutaro0415@outlook.jp>
2022-12-09 14:25:24 +0900

    [update] コルーチンの勉強

--- Assets/Script/SampleCoroutine.cs
+++ Assets/Script/SampleCoroutine.cs
@@ -1,24 +1,111 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SampleCoroutine : MonoBehaviour
 {
-    void Start()
+    bool _isNext;
+    private void Start()
     {
-        var r = RotateAsync(100);
-        while(r.MoveNext())
+        StartCoroutine(ExecuteAsync());
+    }
+
+    private IEnumerator ExecuteAsync()
+    {
+        Debug.Log("ExecuteAsync: Begin");
+
+        while (true)
         {
-            RotateAsync(1);
+            var t = 0F;
+
+            // XŽ²‚Ì‰ñ“]‚ð2•bŠÔ
+            yield return RotateAsync(Vector3.right, 2);
+
+            // 1•bŠÔ‘Ò‹@‚·‚é
+            //yield return new WaitForSeconds(1);
+
+            //yield return WaitForSecondOrCrick();
+
+            yield return AllWait();
+
+            // YŽ²‚Ì‰ñ“]‚ð2•bŠÔ
+            yield return RotateAsync(Vector3.up, 2);
+
+            // 1•bŠÔ‘Ò‹@‚·‚é
+            yield return new WaitForSeconds(1);
+
+            // ZŽ²‚Ì‰ñ“]‚ð2•bŠÔ
+            yield return RotateAsync(Vector3.forward, 2);
+
+            // 1•bŠÔ‘Ò‹@‚·‚é
+            yield return new WaitForSeconds(1);
+        }
+
+        // Debug.Log("ExecuteAsync: End");
+    }
+
+    private IEnumerator RotateAsync(Vector3 eulers, float duration)
+    {
+        Debug.Log($"RotateAsync: Begin eulers={eulers}, duration={duration}");
+        var t = 0F;
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            transform.Rotate(eulers);
+            yield return null;
+        }
+        Debug.Log("ExecuteAsync: End");
+    }
+
+
+    IEnumerator WaitForSecondOrCrick()
+    {
+        float time = 0;
+        while (5 > time)
+        {
+            yield return null;
+            time += Time.deltaTime;
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Fire1");
+                yield break;
+            }
         }
     }
 
-    private IEnumerator RotateAsync(int num)
+    IEnumerator WaitForSecond(int second)
     {
-        for (int i = 0; i < num; i++)
+        yield return null;
+        float time = 0;
+
+        time += Time.deltaTime;
+        if (time > second)
         {
-            transform.Rotate(i, 0, 0);
-            yield return null;
+            _isNext = true;
         }
+
+    }
+
+    IEnumerator WaitClick()
+    {
+        yield return null;
+        if (Input.GetMouseButtonDown(0))
+        {
+            _isNext = true;
+            yield break;
+        }
+    }
+
+    IEnumerator AllWait()
+    {
+        while (!_isNext)
+        {
+            yield return null;
+            WaitForSecond(2);
+            WaitClick();
+        }
+    }
+    private void Update()
+    {
+        Debug.Log(_isNext);
     }
 }
